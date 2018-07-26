0433ec02e3bf3d35b91eaa7b2760855b541e107e
rmh410 <rmh410@nyu.edu>
2018-07-26 15:36:34 -0400

    player scripting

--- Assets/Scripts/ClientScript.cs
+++ Assets/Scripts/ClientScript.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class ClientScript : MonoBehaviour {
 
+	public bool inGame;
+	public GameObject myPlayer;
+	public GameObject playerSpawn;
+
 	// Use this for initialization
 	void Start () {
+		// instantiate a player prefab at the player spawn
+
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// if not in a game look for one
+		if(!inGame) {
+			// ping for a host if close to a second
+			if(Time.fixedTime - (int)Time.fixedTime < 0.05f) {
+
+			}
+
+
+		}
+
+
+		// actual gameplay
+		else {
+			// move our player capsule to correct location
+			Vector3 dest = Camera.main.transform.position;
+			dest.y = dest.y - 0.4f;
+			myPlayer.transform.position = dest;
+
+			// 
+
+		}
 		
 	}
 }
