22837d6ecd664d5e01cb223224b0e05794156d8e
Wagner Colodette <wmvcolodette@gmail.com>
2018-07-26 15:36:06 -0400

    Create CarController.cs and TrafficController.cs

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+	private static Color[] paint = { Color.blue, Color.cyan, Color.green, Color.red, Color.yellow, Color.magenta };
+	private static int lastPaintIndex = paint.Length - 1;
+	private float speed = 5.0f;
+
+	void Start()
+	{
+		// Assign random color to vehicle
+		Color randomColor = paint[(int)Random.Range(0.0f, lastPaintIndex - 1)];
+		GameObject.Find("Body Top").GetComponent<Renderer>().material.color = randomColor;
+		GameObject.Find("Body Bottom").GetComponent<Renderer>().material.color = randomColor;
+	}
+
+	void Update()
+	{
+		transform.position += transform.forward * Time.deltaTime * speed;
+	}
+}
