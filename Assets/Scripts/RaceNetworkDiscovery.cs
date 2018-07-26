12e427887c94e625c9bc1be176da1773f4fae3dd
rmh410 <rmh410@nyu.edu>
2018-07-26 19:11:40 -0400

    added trivial networking

---
+++ Assets/Scripts/RaceNetworkDiscovery.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RaceNetworkDiscovery : NetworkDiscovery {
+	private bool inGame = false;
+
+	// Use this for initialization
+	void Start () {
+		Initialize();
+	}
+	
+	public override void OnReceivedBroadcast(string fromAddress, string data) {
+		// if we are not in a game should be trying to join
+		if(!inGame) {
+			base.OnReceivedBroadcast(fromAddress, data);
+			Debug.Log("FromAddress: " + fromAddress + " Data: " + data);
+			string[] addressSplit = fromAddress.Split(':');
+			string[] dataSplit = data.Split(';');
+			NetworkManager.singleton.networkAddress = addressSplit[addressSplit.Length-1];
+			NetworkManager.singleton.networkPort = int.Parse(dataSplit[dataSplit.Length-1]);
+			NetworkManager.singleton.StartClient();
+		}
+	}
+
+	public void StartListeningBroadcast() {
+		StartAsClient();
+	}
+
+	public void StartAsHost() {
+		NetworkManager.singleton.StartHost();
+		StartAsServer();
+	}
+}
